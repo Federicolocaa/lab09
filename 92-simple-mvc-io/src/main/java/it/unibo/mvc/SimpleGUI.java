fdda2bf4576e9c1aa40167be09ef60576067bc6a
Federico Locatelli <federicolocatelli@MacBook-Air-di-Federico.local>
2025-11-18 14:42:45 +0100

    Completato l'esercizio 92-simple-mvc-io, Tutto funziona correttamente e rispetta i check.

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,45 +1,51 @@
 package it.unibo.mvc;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 
 /**
  * A very simple program using a graphical interface.
- * 
  */
 public final class SimpleGUI {
 
     private static final String TITLE = "My First Java Graphical Interface";
-    private final JFrame frame = new JFrame(TITLE);
-    final Controller controller = new Controller();
     private static final int PROPORTION = 5;
 
+    private final JFrame frame = new JFrame(TITLE);
+    private final Controller controller = new Controller();
+
+    /**
+     * Builds a new SimpleGUI.
+     */
     public SimpleGUI() {
         final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
         final JTextArea area = new JTextArea();
         final JButton save = new JButton("Save");
-        canvas.add(area);
+        canvas.add(area, BorderLayout.CENTER);
         canvas.add(save, BorderLayout.SOUTH);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        /*
+         * Handlers
+         */
         save.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent ignored) {
                 try {
                     controller.save(area.getText());
-                    area.setText("");
-                } catch (Exception e) {
+                } catch (final IOException e) {
                     JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
                     e.printStackTrace(); // NOPMD
                 }
@@ -61,7 +67,7 @@ public final class SimpleGUI {
      *
      * @param args ignored
      */
-    public static void main(String[] args) {
-       new SimpleGUI().display();
+    public static void main(final String... args) {
+        new SimpleGUI().display();
     }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -23,9 +23,9 @@ public final class SimpleGUIWithFileChooser {
 
     private static final String TITLE = "My Second Java Graphical Interface";
     private static final int PROPORTION = 5;
-    
+
     private final JFrame frame = new JFrame(TITLE);
-    private final Controller controller = new Controller(); 
+    private final Controller controller = new Controller();
 
     /**
      * Builds a new GUI.
@@ -35,36 +35,42 @@ public final class SimpleGUIWithFileChooser {
         final JPanel uppPanel = new JPanel();
         canvas.setLayout(new BorderLayout());
         uppPanel.setLayout(new BorderLayout());
-        
+
         final JTextArea area = new JTextArea();
         final JTextField uppArea = new JTextField(controller.getPath());
         uppArea.setEditable(false);
-        
+
         final JButton save = new JButton("Save");
-        final JButton browse = new JButton("Browse..."); 
+        final JButton browser = new JButton("Browse...");
+
         canvas.add(uppPanel, BorderLayout.NORTH);
         canvas.add(area, BorderLayout.CENTER);
         canvas.add(save, BorderLayout.SOUTH);
         uppPanel.add(uppArea, BorderLayout.CENTER);
-        uppPanel.add(browse, BorderLayout.LINE_END);
-        
+        uppPanel.add(browser, BorderLayout.LINE_END);
+
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        /*
+         * Handler per il salvataggio
+         */
         save.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent ignored) {
                 try {
                     controller.save(area.getText());
-                    area.setText("");
-                } catch (final IOException e) { 
+                } catch (final IOException e) {
                     JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
-                    e.printStackTrace(); // NOPMD
+                    e.printStackTrace(); // NOPMD: 
                 }
             }
         });
 
-        browse.addActionListener(new ActionListener() {
+        /*
+         * Handler per il Browse
+         */
+        browser.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent ignored) {
                 final JFileChooser fileChooser = new JFileChooser("Where to save");
@@ -76,8 +82,7 @@ public final class SimpleGUIWithFileChooser {
                     final File newDest = fileChooser.getSelectedFile();
                     controller.setDestination(newDest);
                     uppArea.setText(newDest.getPath());
-                } else if (result == JFileChooser.CANCEL_OPTION) {
-                } else {
+                } else if (result != JFileChooser.CANCEL_OPTION) {
                     JOptionPane.showMessageDialog(frame, result, "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
@@ -98,7 +103,7 @@ public final class SimpleGUIWithFileChooser {
      *
      * @param args ignored
      */
-    public static void main(final String... args) { // Varargs per Checkstyle
-       new SimpleGUIWithFileChooser().display();
+    public static void main(final String... args) {
+        new SimpleGUIWithFileChooser().display();
     }
 }
